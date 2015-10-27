ad1fabb36d82cd0c75e9efc28a9b6ed9acb06a6f
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2015-10-27 17:50:32 +0100

    KnP: add postgrest recipe

--- cookbooks/hotpot/recipes/default.rb
+++ cookbooks/hotpot/recipes/default.rb
@@ -12,3 +12,25 @@ include_recipe "nodejs::npm"
 
 nodejs_npm "express"
 nodejs_npm "express-generator"
+
+include_recipe "postgresql::server"
+include_recipe "postgresql::server_dev"
+include_recipe "postgresql::client"
+include_recipe "postgresql::setup_databases"
+include_recipe "postgresql::setup_users"
+
+postgresql_user "hotpot" do
+  superuser false
+  createdb false
+  login true
+  replication false
+  password "hotpot"
+end
+
+postgresql_database "hotpot" do
+  owner "hotpot"
+  encoding "UTF-8"
+  template "template0"
+  locale "en_US.UTF-8"
+end
+
